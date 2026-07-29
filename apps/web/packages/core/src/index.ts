0d37003efab655644bd32e1960602f92282e3dad
xzerone <info@tooloralabs.com>
2026-07-29 14:50:18 +0300

    feat(core): export core package

---
+++ apps/web/packages/core/src/index.ts
@@ -0,0 +1,18 @@
+export * from "./contracts/tool";
+export * from "./contracts/context";
+export * from "./contracts/metadata";
+export * from "./contracts/result";
+
+export * from "./errors/ToolError";
+export * from "./errors/ValidationError";
+export * from "./errors/ExecutionError";
+
+export * from "./registry/ToolRegistry";
+export * from "./engine/ToolEngine";
+export * from "./pipeline/Pipeline";
+
+export * from "./utils/assert";
+export * from "./utils/isObject";
+export * from "./utils/isString";
+export * from "./utils/isNumber";
+export * from "./utils/isBoolean";
