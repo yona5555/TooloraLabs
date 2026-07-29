07db95ff322d6b8fa17e4eecd425b235fe132d2d
xzerone <info@tooloralabs.com>
2026-07-29 15:06:11 +0300

    feat(sdk): export ToolSchema

--- apps/web/packages/sdk/src/index.ts
+++ apps/web/packages/sdk/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./contracts/BaseTool";
 export * from "./contracts/ToolInput";
 export * from "./contracts/ToolOutput";
+export * from "./contracts/ToolSchema";
 
 export * from "./errors/SDKError";
 export * from "./errors/ValidationError";
